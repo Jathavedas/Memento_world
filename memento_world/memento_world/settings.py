2dbec307630b85b728e357628e04574924b8e2c8
jathavedas <jathavedaskavikumar@gmail.com>
2024-10-27 14:53:35 +0530

    new

--- memento_world/memento_world/settings.py
+++ memento_world/memento_world/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
+STATIC_ROT = os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=BASE_DIR/'images'
 MEDIA_URL='/media/'
 
