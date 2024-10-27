91431ba79a073bbbaeee6951f8866f2f1736016d
jathavedas <jathavedaskavikumar@gmail.com>
2024-10-27 22:31:04 +0530

    Add new images

--- memento_world/memento_world/settings.py
+++ memento_world/memento_world/settings.py
@@ -122,12 +122,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS =[os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-MEDIA_ROOT=BASE_DIR/'images'
-MEDIA_URL='/media/'
+STATIC_URL = '/static/'
 
+# This is where you put additional static files for development
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# This is where Django will collect all static files when you run `collectstatic`
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Configuration for media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
